1168e9892b1fdbf7e0f9e624b00f060fe88bbad1
PPakSang <sanghyun-dev@naver.com>
2022-10-30 15:36:29 +0900

    Feat: Swagger 추가

--- src/main/java/com/example/mapp/place/controller/PlaceRecommendController.java
+++ src/main/java/com/example/mapp/place/controller/PlaceRecommendController.java
@@ -2,19 +2,23 @@ package com.example.mapp.place.controller;
 
 import com.example.mapp.place.dto.KakaoPlaceRecommendRequestDto;
 import com.example.mapp.place.service.KakaoPlaceRecommendService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Tag(name = "명소 추천")
 @RestController
-@RequestMapping("/place")
+@RequestMapping("/api/v1/place")
 @RequiredArgsConstructor
 public class PlaceRecommendController {
 
     private final KakaoPlaceRecommendService kakaoPlaceRecommendService;
 
+    @Operation(summary = "추천 받기")
     @GetMapping("/recommend")
     public Mono<?> recommend(KakaoPlaceRecommendRequestDto dto) {
         return kakaoPlaceRecommendService.recommend(dto);
