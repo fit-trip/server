8b311b88ef5d1b9c0f5e9b872e8262d2e0dac3d8
sami355-24 <ddung355@gmail.com>
2022-11-15 12:41:06 +0900

    Feat: add RouteInfoPerMoney

--- src/main/java/com/example/mapp/entity/RouteInfoPerMoney.java
+++ src/main/java/com/example/mapp/entity/RouteInfoPerMoney.java
@@ -1,5 +1,13 @@
 package com.example.mapp.entity;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "RouteInfoPerMoney")
 public class RouteInfoPerMoney extends RouteInfo {
     Integer money;
 }
