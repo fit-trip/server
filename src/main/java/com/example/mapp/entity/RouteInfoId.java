5f63a489c30eed64d749ed4ff9d338a006c1f825
sami355-24 <ddung355@gmail.com>
2022-11-16 01:43:46 +0900

    Refactor: RouteInfoId

--- src/main/java/com/example/mapp/entity/RouteInfoId.java
+++ src/main/java/com/example/mapp/entity/RouteInfoId.java
@@ -1,10 +1,7 @@
 package com.example.mapp.entity;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
