f52e3b5b0873b3efaefedb6a4dd78da0d9300352
sami355-24 <ddung355@gmail.com>
2022-11-15 12:44:25 +0900

    Feat: add RouteInfoPerTime

--- src/main/java/com/example/mapp/entity/RouteInfoPerTime.java
+++ src/main/java/com/example/mapp/entity/RouteInfoPerTime.java
@@ -1,8 +1,13 @@
 package com.example.mapp.entity;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@NoArgsConstructor
+@Table(name = "RouteInfoPerTime")
 public class RouteInfoPerTime extends RouteInfo{
     Integer time;
 }
