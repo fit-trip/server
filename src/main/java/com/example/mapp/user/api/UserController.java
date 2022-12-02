45567eadd50d06e2f8e52e9a059d78f905c8c3f6
PPakSang <sanghyun-dev@naver.com>
2022-12-02 15:52:54 +0900

    Chore: swagger subscription 추가

--- src/main/java/com/example/mapp/user/api/UserController.java
+++ src/main/java/com/example/mapp/user/api/UserController.java
@@ -2,6 +2,8 @@ package com.example.mapp.user.api;
 
 import com.example.mapp.user.dto.UserDto;
 import com.example.mapp.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@Tag(name = "사용자 관리")
 public class UserController {
     private final UserService userService;
 
     @PostMapping
+    @Operation(summary = "회원 가입")
     public void addUser(@RequestBody UserDto dto) {
         userService.addUser(dto);
     }
