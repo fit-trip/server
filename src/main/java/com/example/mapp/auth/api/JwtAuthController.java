7bb6b372fb370c81d2a20b9bbf08890698fbbf58
PPakSang <sanghyun-dev@naver.com>
2022-12-10 16:52:41 +0900

    Chore: swagger subscription 추가

--- src/main/java/com/example/mapp/auth/api/JwtAuthController.java
+++ src/main/java/com/example/mapp/auth/api/JwtAuthController.java
@@ -3,6 +3,8 @@ package com.example.mapp.auth.api;
 import com.example.mapp.auth.dto.TokenDto;
 import com.example.mapp.auth.service.JwtAuthService;
 import com.example.mapp.user.dto.UserDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "인증 정보(토큰) 관리")
 public class JwtAuthController {
 
     private final JwtAuthService jwtAuthService;
 
     @PostMapping
+    @Operation(summary = "로그인")
     public TokenDto login(@RequestBody UserDto userDto) {
         return jwtAuthService.login(userDto);
     }
--- src/main/java/com/example/mapp/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/mapp/schedule/controller/ScheduleController.java
@@ -4,6 +4,8 @@ import com.example.mapp.schedule.dto.ScheduleRequestDto;
 import com.example.mapp.schedule.dto.ScheduleResponseDto;
 import com.example.mapp.schedule.dto.ScheduleUpdateDto;
 import com.example.mapp.schedule.service.ScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,20 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/schedules")
+@Tag(name = "스케줄(일정)")
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
+    @Operation(summary = "일정 생성")
     @PostMapping
     public void createSchedule(@RequestBody ScheduleRequestDto request) {
         scheduleService.addSchedule(getUserIdFromSecurityContext(), request.getLocations());
     }
 
+    @Operation(summary = "일정 공유 상태 변경")
     @PutMapping
     public void updateScheduleSharedStatus(@RequestBody ScheduleUpdateDto dto) {
         dto.setUserId(getUserIdFromSecurityContext());
         scheduleService.updateSharedStatus(dto);
     }
 
+    @Operation(summary = "공유 일정 조회")
     @GetMapping("/shared")
     public List<ScheduleResponseDto> getAllSharedSchedule() {
         return scheduleService.getAllSharedSchedule();
@@ -41,6 +47,7 @@ public class ScheduleController {
         return (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
+    @Operation(summary = "내 일정 조회")
     @GetMapping
     public List<ScheduleResponseDto> getAllMySchedule() {
         String userId = (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
